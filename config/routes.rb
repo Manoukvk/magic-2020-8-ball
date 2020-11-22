0b19e0c3efd353d168a53441b5e7d26247b21a96
manouk van kuyk <manoukvankuyk@gmail.com>
2020-11-22 17:03:02 +0100

    changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'application#question'
   get 'question', to: 'question#question', as: :question
   get 'answer', to: 'question#answer', as: :answer
 end
